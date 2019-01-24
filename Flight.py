6c6798662d3c02d3d0866ecf7b871a1634532ebd
fernanpcf <40845307+fernanpcf@users.noreply.github.com>
2019-01-24 00:23:34 -0600

    Cambio en la definición de los atributos
    Está buena la idea de mantener la información en diccionarios, pero es un poco redundante el código si se definen primero los datos como atributos y posteriormente se trasladan a un diccionario. En la propuesta que hago, la información entra de una vez en forma de diccionario y cada instancia de la clase Flight, posee un solo atributo, el cual es un diccionario con toda la información.

--- Flight.py
+++ Flight.py
@@ -1,25 +1,34 @@
 class Flight:
-      def __init__(self,hasPassengers,destination,departureTime,arrivingTime,touristCost,businessCost,firstClassCost,touristAvailableSeats,businessAvailableSeats,firstClassAvailableSeats,departureDate):
-            self.hasPassengers = hasPassengers
-            self.destination = destination
-            self.departureTime = departureTime
-            self.arrivingTime = arrivingTime
-            self.touristCost = touristCost
-            self.businessCost = businessCost
-            self.firstClassCost = firstClassCost
-            self.touristAvailableSeats = touristAvailableSeats
-            self.businessAvailableSeats = businessAvailableSeats
-            self.firstClassAvailableSeats = firstClassAvailableSeats
-            self.departureDate = departureDate
+	def __init__(self,flightdata):
+		self.flightdata = flightdata #Flight data es un atributo en forma de diccionario
+		
+	@classmethod  #Con un método de clase, puede crearse una instancia empleando una función, gracias a que esta no emplea al self
+	def fcreator(cls,x,flightRecord =[]):
+		for j in range(x):
+			data_descriptor = ['Destino','Fecha de salida (DD/MM/AAAA)','Hora de salida (HH:MM)','Hora de llegada (HH:MM)','Costo clase turista','Cupo clase turista','Costo clase negocios','Cupo clase negocios','Costo primera clase','Cupo primera clase']
+			data_filler = []
+			print(f'\nIngrese datos de vuelo {j+1}:\n')
 
-      def toString(self):
-            return ("\t\tDestino: "+self.destination
-            	+"\n\t\tHora de salida: "+self.departureTime
-            	+"\n\t\tHora de llegada: "+self.arrivingTime
-            	+"\n\t\tCosto de viaje Turista: "+str(self.touristCost)
-            	+"\n\t\tCosto de viaje Negocios: "+str(self.businessCost)
-            	+"\n\t\tCosto de viaje Primera Clase: "+str(self.firstClassCost)
-            	+"\n\t\tLugares disponibles calse Turista: "+str(self.touristAvailableSeats)
-            	+"\n\t\tLugares disponibles calse Negocios: "+str(self.businessAvailableSeats)
-            	+"\n\t\tLugares disponibles calse Primera Clase: "+str(self.firstClassAvailableSeats)
-            	+"\n\t\tFecha de salida: "+self.departureDate)
+			for i in range(len(data_descriptor)):
+				info = input(f"Ingrese {data_descriptor[i]}: ")
+				data_filler.append(info)
+
+			flight_dict = dict(zip(data_descriptor,data_filler))
+			fdata = Flight(flight_dict)
+			flightRecord.append(fdata)
+		return flightRecord
+
+	def displayInfo(self): # Método de objeto, para imprimir los datos de cada vuelo (instancia)
+		for k,v in self.flightdata.items():
+			print('\t'+k +': ',self.flightdata[k]+'\n')
+
+	@staticmethod # Método estático, para poder manipular la variable obj, puesto que esta es solo un almacenador de instancias, no una instancia en sí.
+	def extractObject(element):
+		for i in range(len(element)):
+			print(f'\nVuelo {i+1}:')
+			element[i].displayInfo()  # El método estático, invoca al método de objeto para extraer la info de cada instancia.
+
+n= int(input('Número de vuelos a registrar: '))
+print()
+obj = Flight.fcreator(n) #Se asigna la lista de objetos a la variable obj
+Flight.extractObject(obj) #Imprime cada vuelo con su información
